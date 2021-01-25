3933016cbc384151ed5f3cc49f714a712e451ad8
coffee-dan <coffee-dan@protonmail.com>
2021-01-24 23:56:58 -0600

    Simple tic tac toe game logic

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  squares: string[];
+  xIsNext: boolean;
+  winner: string | null;
+
+  constructor() {
+    this.squares = [];
+    this.xIsNext = true;
+    this.winner = null;
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill('');
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    // modified from the original code to not include null values for squares
+    if (this.squares[idx] === '') {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  // taken mostly from tic tac toe react tutorial and Fireship
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+
+    return null;
+  }
+}
